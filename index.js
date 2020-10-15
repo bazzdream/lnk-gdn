5e4c0e83699d9aeb98955e0593c941697952d48f
Oliver Barabas <oliverbarabas@Olivers-Mini.fritz.box>
2020-10-16 00:53:33 +0200

    feat: create short urls; not using db right now

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const yup = require('yup');
 const monk = require('monk');
 const morgan = require('morgan');
 const cors = require('cors');
+const {nanoid} = require('nanoid');
 
 const app = express()
 
@@ -21,9 +22,44 @@ app.use(express.static('./public'))
 //     // TODO: redirect to url
 // })
 
-// app.post('/url', (req, res) => {
-//     // TODO: create url
-// })
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required()
+})
+
+app.post('/url', async (req, res, next) => {
+    let {slug, url} = req.body;
+
+    try {
+        await schema.validate({
+            slug,
+            url
+        });
+
+        if(!slug) {
+            slug = nanoid(5)
+        }
+        slug = slug.toLowerCase();   
+        res.json({
+            slug,
+            url
+        });   
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.use((error, req, res, next) => {
+    if(error.status) {
+        res.status(error.status)
+    } else {
+        res.status(500)
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack
+    })
+})
 
 const port = process.env.PORT || 1337
 
